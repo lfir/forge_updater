b66121d784b21b7c977f475b25240c9ed1c9c3d1
Astaroth <asta666@xfiles.localdomain>
2017-03-02 18:03:44 -0300

    update variables

--- forge-updater.sh
+++ forge-updater.sh
@@ -3,9 +3,9 @@
 
 replaceIfAccepted() {
 if [[ -n "$1" ]] ; then
-	cp $localDir/forge.profile.properties ~/Downloads/Forge &> /dev/null
-	rm -r $localDir
-	mv ~/Downloads/Forge $localDir
+	cp "$localDir/forge.profile.properties" ~/Downloads/Forge &> /dev/null
+	rm -r "$localDir"
+	mv ~/Downloads/Forge "$localDir"
 	echo "All done."
 else
 	echo "Old version not replaced. The new version can be found in '~/Downloads/Forge'."
@@ -14,8 +14,8 @@ fi
 
 #The first time the updater is run register the local game directory.
 if ! [[ -f ~/.forge-updater ]] ; then
-read -p "Enter the full path to the Forge directory: " orgDir
-echo $orgDir > ~/.forge-updater
+read -pr "Enter the full path to the Forge directory: " orgDir
+echo "$orgDir" > ~/.forge-updater
 fi
 
 #Get the latest available version from the forge download site.
@@ -25,7 +25,7 @@ grep -Po '(?<==")\d+.\d+.\d+(?=/")' /tmp/fv | tail -1 > /tmp/fvl
 localDir=$(cat ~/.forge-updater)
 baseUrl="http://www.cardforge.link/releases/forge/forge-gui-desktop"
 latestVersion=$(cat /tmp/fvl)
-localVInt=$(grep -Po 'ver \K\d+.\d.+\d+$' $localDir/CHANGES.txt | sed 's/\.//g')
+localVInt=$(grep -Po 'ver \K\d+.\d.+\d+$' "$localDir/CHANGES.txt" | sed 's/\.//g')
 latestVInt=$(sed 's/\.//g' /tmp/fvl)
 
 #Check whether the local version is older than the available one.
@@ -34,9 +34,9 @@ if (( localVInt < latestVInt )) ; then
 	downloadUrl="$baseUrl/$latestVersion/forge-gui-desktop-$latestVersion.tar.bz2"
 	mkdir -p ~/Downloads/Forge
 	echo "Downloading new version. Please wait."
-	wget -qO - $downloadUrl | tar xjC ~/Downloads/Forge -f - && \
-	read -p "Replace local version with new version? [Y/N]: " answer
-	replaceIfAccepted $(echo $answer | grep -oi y)
+	wget -qO - "$downloadUrl" | tar xjC ~/Downloads/Forge -f - && \
+	read -pr "Replace local version with new version? [Y/N]: " answer
+	replaceIfAccepted "$(echo "$answer" | grep -oi y)"
 else
 	echo "Forge is up to date."
 fi
