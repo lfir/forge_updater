dd790fd69091823bc7deaf8ba653a233366c9407
Astaroth <psljp@protonmail.com>
2017-07-05 22:23:54 -0300

    safe paths; clear expansions

--- forge-updater.sh
+++ forge-updater.sh
@@ -3,7 +3,7 @@
 
 replaceIfAccepted() {
 if [[ -n "$1" ]] ; then
-	cp "$localDir/forge.profile.properties" "$ldd" &> /dev/null
+	cp "${localDir}/forge.profile.properties" "$ldd" &> '/dev/null'
 	rm -r "$localDir"
 	mv "$ldd" "$localDir"
 	echo "All done."
@@ -13,26 +13,26 @@ fi
 }
 
 #The first time the updater is run register the local game directory.
-if ! [[ -f ~/.forge-updater ]] ; then
+if ! [[ -f "${HOME}/.forge-updater" ]] ; then
 	read -rp "Enter the full path to the Forge directory: " orgDir
-	echo "$orgDir" > ~/.forge-updater
+	echo "$orgDir" > "${HOME}/.forge-updater"
 fi
 
 #Get the latest available version from the forge download site.
-baseUrl="https://releases.cardforge.org/forge/forge-gui-desktop"
-wget -qO /tmp/fv "$baseUrl"
-grep -Po '(?<==")\d+.\d+.\d+(?=/")' /tmp/fv | tail -1 > /tmp/fvl
+baseUrl='https://releases.cardforge.org/forge/forge-gui-desktop'
+wget -qO '/tmp/fv' "$baseUrl"
+grep -Po '(?<==")\d+.\d+.\d+(?=/")' "/tmp/fv" | tail -1 > '/tmp/fvl'
 
 ldd=$(xdg-user-dir DOWNLOAD)/Forge
-localDir=$(cat ~/.forge-updater)
-latestVersion=$(cat /tmp/fvl)
+localDir=$(cat "${HOME}/.forge-updater")
+latestVersion=$(cat '/tmp/fvl')
 localVInt=$(grep -Po '\-\K\d+.\d.+\d+' "$localDir/forge.sh" | sed 's/\.//g')
-latestVInt=$(sed 's/\.//g' /tmp/fvl)
+latestVInt=$(sed 's/\.//g' '/tmp/fvl')
 
 #Check whether the local version is older than the available one.
 #Ask the user if he wants to replace the local version with the one downloaded.
 if (( localVInt < latestVInt )) ; then
-	downloadUrl="$baseUrl/$latestVersion/forge-gui-desktop-$latestVersion.tar.bz2"
+	downloadUrl="${baseUrl}/${latestVersion}/forge-gui-desktop-${latestVersion}.tar.bz2"
 	mkdir -p "$ldd"
 	echo "Downloading new version. Please wait."
 	wget -qO - "$downloadUrl" | tar xjC "$ldd" -f - && \
@@ -43,4 +43,4 @@ else
 fi
 
 #Erase auxiliary files.
-rm /tmp/fv /tmp/fvl
+rm '/tmp/fv' '/tmp/fvl'
